05a14dc011fbf03ec5953ab453e17b8c328542a7
Ben Getsug <ben@shift31.com>
2014-05-18 04:56:02 -0500

    more refactoring

--- app/src/Host/HostRepository.php
+++ app/src/Host/HostRepository.php
@@ -4,18 +4,4 @@
 interface HostRepository
 {
 
-    /**
-     * @param array $data
-     *
-     * @return void
-     */
-    public function encryptAdminPassword(array &$data);
-
-
-    /**
-     * @param array $data
-     *
-     * @return void
-     */
-    public function decryptAdminPassword(array &$data);
 }
--- app/src/Host/HostService.php
+++ app/src/Host/HostService.php
@@ -2,7 +2,10 @@
 
 use Hostbase\Entity\Entity;
 use Hostbase\Entity\EntityService;
+use Hostbase\Entity\Exceptions\InvalidEntity;
 use Hostbase\Entity\MakesEntities;
+use Crypt;
+use Log;
 
 
 class HostService implements EntityService, MakesEntities {
@@ -32,11 +35,19 @@ class HostService implements EntityService, MakesEntities {
 
     /**
      * @param null|string $id
-     * @return Entity
+     * @return Host
      */
     public function showOne($id)
     {
-        return $this->repository->getOne($id);
+        $host = $this->repository->getOne($id);
+
+        $data = $host->getData();
+
+        $this->decryptAdminPassword($data);
+
+        $host->setData($data);
+
+        return $host;
     }
 
 
@@ -46,7 +57,17 @@ class HostService implements EntityService, MakesEntities {
      */
     public function showMany(array $ids)
     {
-        return $this->repository->getMany($ids);
+        $hosts = $this->repository->getMany($ids);
+
+        foreach ($hosts as &$host) {
+            $data = $host->getData();
+
+            $this->decryptAdminPassword($data);
+
+            $host->setData($data);
+        }
+
+        return $hosts;
     }
 
 
@@ -88,24 +109,56 @@ class HostService implements EntityService, MakesEntities {
 
 
     /**
-     * @param Entity $entity
-     *
-     * @return Entity
+     * @param Entity $host
+     * @return Host
+     * @throws InvalidEntity
+     * @throws HostMissingFqdn
      */
-    public function store(Entity $entity)
+    public function store(Entity $host)
     {
-        return $this->repository->store($entity);
+        if (! $host instanceof Host) {
+            throw new InvalidEntity('Expected $host to be an instance of Host');
+        }
+
+        $fqdn = $host->getFqdn();
+
+        if ($fqdn === null) {
+            throw new HostMissingFqdn("Host must have a value for 'fqdn'");
+        }
+
+        $data = $host->getData();
+
+        // generate hostname and domain if they don't already exist
+        if (!isset($data['hostname']) && !isset($data['domain'])) {
+            $fqdnParts = explode('.', $data['fqdn'], 2);
+            //Log::debug('$fqdnParts:' . print_r($fqdnParts, true));
+            $data['hostname'] = $fqdnParts[0];
+            $data['domain'] = $fqdnParts[1];
+        }
+
+        // encrypt admin password
+        $this->encryptAdminPassword($data);
+
+        $host->setData($data);
+
+        return $this->repository->store($host);
     }
 
 
     /**
-     * @param Entity $entity
-     *
-     * @return Entity
+     * @param Entity $host
+     * @return Host
      */
-    public function update(Entity $entity)
+    public function update(Entity $host)
     {
-        return $this->repository->update($entity);
+        $data = $host->getData();
+
+        // encrypt admin password
+        $this->encryptAdminPassword($data);
+
+        $host->setData($data);
+
+        return $this->repository->update($host);
     }
 
 
@@ -118,5 +171,30 @@ class HostService implements EntityService, MakesEntities {
     public function destroy($id)
     {
         return $this->repository->destroy($id);
+    }
+
+
+    /**
+     * @param array $data
+     */
+    protected function encryptAdminPassword(array &$data)
+    {
+        if (isset($data['adminCredentials'])) {
+            $data['adminCredentials']['encryptedPassword'] = Crypt::encrypt($data['adminCredentials']['password']);
+            unset($data['adminCredentials']['password']);
+        }
+    }
+
+
+    /**
+     * @param array $data
+     */
+    protected function decryptAdminPassword(array &$data)
+    {
+        if (isset($data['adminCredentials'])) {
+            Log::debug("Decrypting admin password for {$data['fqdn']}");
+            $data['adminCredentials']['password'] = Crypt::decrypt($data['adminCredentials']['encryptedPassword']);
+            unset($data['adminCredentials']['encryptedPassword']);
+        }
     }
 } 
